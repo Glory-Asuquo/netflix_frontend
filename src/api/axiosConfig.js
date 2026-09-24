b72a3946a08307e7505b3692c5f7427bcf8b3c0a
Glory Victor Asuquo <gloryva0@gmail.com>
2026-09-24 09:00:22 +0100

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.16.76.30:8080',
+    baseURL:'http://3.143.23.134',
     headers: {
         'Content-Type': 'application/json',
     },
