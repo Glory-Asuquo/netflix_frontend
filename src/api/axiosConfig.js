def5d49193b458fa98aaa0f470105618ced5b18a
Glory Victor Asuquo <gloryva0@gmail.com>
2026-09-24 18:19:09 +0100

    Update axios base URL to new server address

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.143.23.134',
+    baseURL:'http://3.148.250.222:8080',
     headers: {
         'Content-Type': 'application/json',
     },
